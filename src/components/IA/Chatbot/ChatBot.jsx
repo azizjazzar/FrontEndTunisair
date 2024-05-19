8aabc13e467255ae3e362981a376edfadca1bcf3
hibachebbi2 <chebbihabou@gmail.com>
2024-05-19 19:18:23 +0100

    update

--- src/components/IA/Chatbot/ChatBot.jsx
+++ src/components/IA/Chatbot/ChatBot.jsx
@@ -1,8 +1,7 @@
-// Chatbot.js
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Chatbot.css';
+
 const fetchResponseFromAPI = async (trans) => {
     try {
         const res = await axios.post(
@@ -15,44 +14,19 @@ const fetchResponseFromAPI = async (trans) => {
         return "Une erreur s'est produite lors de la récupération de la réponse.";
     }
 };
+
 const Chatbot = () => {
     const [userMessage, setUserMessage] = useState('');
     const [botResponses, setBotResponses] = useState([]);
-    const [currentOptions, setCurrentOptions] = useState([]);
     const [isChatbotOpen, setIsChatbotOpen] = useState(false);
     const [suggestedQuestions, setSuggestedQuestions] = useState([]);
     const [permatranscript, setPermatranscript] = useState("");
+    const [error, setError] = useState(null);
 
     const allQuestions = [
         "Qu'est-ce que Tunisair ?",
         "Comment réserver un vol avec Tunisair ?",
-        "Quels sont les services offerts par Tunisair ?",
-        "Comment puis-je modifier ma réservation ?",
-        "Quelles sont les politiques de bagages de Tunisair ?",
-        "Comment puis-je annuler mon vol ?",
-        "Comment obtenir un remboursement ?",
-        "Quels sont les moyens de paiement acceptés par Tunisair ?",
-        "Comment puis-je contacter le service client de Tunisair ?",
-        "Quels sont les avantages du programme de fidélité Fidelys ?",
-        "Comment puis-je m'inscrire au programme de fidélité Fidelys ?",
-        "Quels documents sont nécessaires pour voyager ?",
-        "Comment puis-je m'enregistrer en ligne ?",
-        "Comment fonctionne le système de points Fidelys ?",
-        "Comment puis-je ajouter des services supplémentaires à ma réservation ?",
-        "Quels sont les services disponibles en vol ?",
-        "Comment puis-je signaler un bagage perdu ?",
-        "Quelles sont les destinations desservies par Tunisair ?",
-        "Comment obtenir des informations sur les vols en temps réel ?",
-        "Quels sont les frais pour les modifications de réservation ?",
-        "Quelles sont les options de repas à bord ?",
-        "Comment puis-je réserver un siège spécifique ?",
-        "Quels sont les droits des passagers en cas de retard ou d'annulation ?",
-        "Comment gérer ma réservation en ligne ?",
-        "Quels sont les services pour les passagers à mobilité réduite ?",
-        "Comment vérifier les exigences de visa pour ma destination ?",
-        "Comment puis-je obtenir des informations sur les vols en correspondance ?",
-        "Quels sont les services offerts aux voyageurs fréquents ?",
-        "Comment puis-je m'inscrire aux alertes de vol ?",
+        // Autres questions...
         "Comment puis-je obtenir un certificat de vol ?"
     ];
 
@@ -64,16 +38,70 @@ const Chatbot = () => {
         try {
             const response = await axios.get('https://colabhub.onrender.com/api/chatbot/initial');
             setBotResponses([{ type: 'bot', content: response.data.response }]);
-            setCurrentOptions(response.data.options ?? []);
         } catch (error) {
             console.error('Error fetching initial message:', error);
+        }
+    };
+    const handleGenerateText = async (text) => {
+        try {
+            const requestBody = {
+                contents: [
+                    {
+                        parts: [{ text: text }],
+                    },
+                ],
+                generationConfig: {
+                    temperature: 1,
+                    topK: 0,
+                    topP: 0.95,
+                    maxOutputTokens: 8192,
+                    stopSequences: [],
+                },
+                safetySettings: [
+                    {
+                        category: 'HARM_CATEGORY_HARASSMENT',
+                        threshold: 'BLOCK_MEDIUM_AND_ABOVE',
+                    },
+                    {
+                        category: 'HARM_CATEGORY_HATE_SPEECH',
+                        threshold: 'BLOCK_MEDIUM_AND_ABOVE',
+                    },
+                    {
+                        category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
+                        threshold: 'BLOCK_MEDIUM_AND_ABOVE',
+                    },
+                    {
+                        category: 'HARM_CATEGORY_DANGEROUS_CONTENT',
+                        threshold: 'BLOCK_MEDIUM_AND_ABOVE',
+                    },
+                ],
+            };
+
+            const response = await axios.post(
+                'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro-latest:generateContent?key=AIzaSyDgvnVb9fKln74JtqCgfkCvouLYlYK4gEc',
+                requestBody
+            );
+
+            if (response.status === 200) {
+                const generatedText = response.data.candidates[0].content.parts[0].text;
+                return generatedText;
+            } else {
+                setError('An error occurred during the request to Google Gemini');
+            }
+        } catch (error) {
+            setError('An error occurred during the request to Google Gemini');
+            console.error('Error during request to Google Gemini:', error);
         }
     };
 
     const sendMessage = async () => {
         if (userMessage.trim() !== '') {
             try {
-                const response = await fetchResponseFromAPI("Donc, je t'explique : vous êtes un assistant vocal pour Tunisair. Réponds-moi sans utiliser (*). Voici les dialogues précédents (si c'est vide, ignore) : " + userMessage);
+                const response = await handleGenerateText(
+                    "Donc, je t'explique : vous êtes un assistant vocal pour Tunisair. Réponds-moi sans utiliser (*). Voici les dialogues précédents (si c'est vide, ignore) : " +
+                    permatranscript + ". Voici le dialogue actuel : " + userMessage
+                );
+                setPermatranscript(permatranscript + " " + userMessage);
                 setBotResponses(prevResponses => [
                     ...prevResponses,
                     { type: 'user', content: userMessage }
@@ -84,47 +112,37 @@ const Chatbot = () => {
                 ]);
                 setUserMessage('');
                 setSuggestedQuestions([]);
-                if (response.data.options) {
-                    setCurrentOptions(response.data.options);
-                } else {
-                    setCurrentOptions([]);
-                }
 
                 // Check for specific messages and apply redirection
-                if (response.data.response.includes("je vais vous diriger vers la page consultation")) {
-                    // Delayed redirection after 3 seconds
+                if (response.includes("je vais vous diriger vers la page consultation")) {
+                    // Delayed redirection after 6 seconds
                     setTimeout(() => {
                         window.location.href = '/do-a-quick-consultation';
                     }, 6000);
-                } else if (response.data.response.includes("je vais vous diriger vers la page blog")) {
-                    // Delayed redirection after 3 seconds
+                } else if (response.includes("je vais vous diriger vers la page blog")) {
+                    // Delayed redirection after 6 seconds
                     setTimeout(() => {
                         window.location.href = '/blog';
                     }, 6000);
-                } else if (response.data.response.includes("je vais vous diriger vers la page collaboration")) {
-                    // Delayed redirection after 3 seconds
+                } else if (response.includes("je vais vous diriger vers la page collaboration")) {
+                    // Delayed redirection after 6 seconds
                     setTimeout(() => {
                         window.location.href = '/collaboration';
                     }, 6000);
-                } else if (response.data.response.includes("je vais vous diriger vers la page service")) {
-                    // Delayed redirection after 3 seconds
+                } else if (response.includes("je vais vous diriger vers la page service")) {
+                    // Delayed redirection after 6 seconds
                     setTimeout(() => {
                         window.location.href = '/buyProject';
                     }, 6000);
                 }
-
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         }
     };
 
-    const handleOptionSelect = (option) => {
-        sendMessage(option);
-    };
-
     const toggleChatbot = () => {
-        setIsChatbotOpen(true);
+        setIsChatbotOpen(!isChatbotOpen);
     };
 
     const handleKeyDown = (e) => {
@@ -149,13 +167,12 @@ const Chatbot = () => {
     };
 
     return (
-        <div className="chatbot-container">
-            {!isChatbotOpen && (
-                <button className="chatbot-button" onClick={toggleChatbot}>
-                    <img className="bg-white" src="https://th.bing.com/th/id/R.2063815249cc5fd9c8de1ca8c88938c0?rik=YnKWCQ%2bRY%2bRJIw&pid=ImgRaw&r=0" alt="Bot Icon" />
+        <div>
+            {!isChatbotOpen ? (
+                <button onClick={toggleChatbot}>
+                    <img className="chatbot-button" src="/img/chatbot.png" alt="Bot Icon" />
                 </button>
-            )}
-            {isChatbotOpen && (
+            ) : (
                 <div className="chat-window">
                     <div className="chat-header">
                         <div className="avatar">
@@ -165,11 +182,11 @@ const Chatbot = () => {
                             <div className="bot-name">Tunisair Bot</div>
                             <div className="bot-status">Online</div>
                         </div>
-                        <button className="close-button" onClick={() => setIsChatbotOpen(false)}>X</button>
+                        <button className="close-button" onClick={toggleChatbot}>X</button>
                     </div>
                     <div className="chat-messages">
                         {botResponses.slice().reverse().map((message, index) => (
-                            <div key={index} className={message.type === 'user' ? 'user-message' : 'bot-message'}>
+                            <div key={index} className={`message ${message.type === 'user' ? 'user-message' : 'bot-message'}`}>
                                 {message.type === 'bot' && (
                                     <div className="avatar">
                                         <img className="bg-white" src="https://th.bing.com/th/id/R.2063815249cc5fd9c8de1ca8c88938c0?rik=YnKWCQ%2bRY%2bRJIw&pid=ImgRaw&r=0" alt="Bot Avatar" />
